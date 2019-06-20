5e2656dd78744502b768bafc102ed4c185b3a427
juan <juanmc32145@gmail.com>
2019-06-20 15:00:00 -0500

    hola mundo

---
+++ hm.java
@@ -0,0 +1,16 @@
+
+package javaapplication21;
+
+import javax.swing.*;
+
+public class JavaApplication21 {
+
+    
+    public static void main(String[] args) {
+       
+
+        JOptionPane.showMessageDialog(null, "hola mundo");
+
+    }
+
+}
